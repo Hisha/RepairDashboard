281dfac23d5c4209b152bc4baf8c207e134ea538
Hisha <vndinmchine@gmail.com>
2026-03-06 12:54:05 -0500

    Added bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+// Absolute path to application root
+define('APP_ROOT', __DIR__);
+
+// Optional helpers
+define('BIN_PATH', APP_ROOT . '/bin');
+define('MODEL_PATH', APP_ROOT . '/bin/Model');
+
+?>
