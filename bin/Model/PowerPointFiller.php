d76f68477d6b95813fc1df285b8f6227beb4b51a
Hisha <vndinmchine@gmail.com>
2026-03-09 12:44:47 -0400

    Query update PowerPointFiller

--- bin/Model/PowerPointFiller.php
+++ bin/Model/PowerPointFiller.php
@@ -3,11 +3,20 @@ include_once APP_ROOT . '/bin/Utilities/db.php';
 
 class PowerPointFiller
 {
+    /*
+     * `id` INT NOT NULL AUTO_INCREMENT,
+	 * `program` VARCHAR(50) NOT NULL,
+	 * `title` VARCHAR(50) NOT NULL,
+	 * `pm` VARCHAR(50) NOT NULL,
+	 * `programname` VARCHAR(50) NOT NULL
+     */
+    protected $returnString;
+    protected $_returnFields = 'program,title,pm,programname';
     protected $_tableName = 'powerpoint_filler';
     
     public function getPPFiller(string $selectedProgram){
         $db = new db();
-        $this->returnString = "SELECT * FROM $this->_tableName WHERE program = $selectedProgram";
+        $this->returnString = "SELECT $this->_returnFields FROM $this->_tableName WHERE program = $selectedProgram";
         $getAll = $db->query($this->returnString)->fetchAll();
         $db->close();
         return $getAll;
