43dfd2abf5de96741ff275c451523b24f09bdb10
Kevin Smith <vndinmchine@gmail.com>
2026-03-09 10:31:42 -0400

    Initialize monthly_reqs.php with basic HTML structure

--- monthly_reqs.php
+++ monthly_reqs.php
@@ -1,1 +1,17 @@
-
+<?php
+require_once __DIR__ . '/bootstrap.php';
+include 'menu.php';
+?>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Monthly Reqs</title>
+</head>
+<body>
+	<table>
+		<tr>
+			<td align="center"></td>
+		</tr>
+	</table>
+</body>
+</html>
