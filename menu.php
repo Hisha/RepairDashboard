eb2117fc6a858f862a897c9138dbe43fc5a68018
Hisha <vndinmchine@gmail.com>
2026-03-09 10:10:28 -0400

    Changing style

--- menu.php
+++ menu.php
@@ -1,6 +1,13 @@
 <?php
 ?>
-<table style="background-color: #808080; width: 100%;">
+<style>
+table {
+  border-spacing: 40px;
+  background-color: #808080;
+  width: 100%;
+}
+</style>
+<table>
 	<tr>
 		<a href="index.php">Home</a>
 		<a href="upload_excel.php">Upload Link</a>
