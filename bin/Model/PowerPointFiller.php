96720afe43badc0e1d9f3a6c768afc91e3fc15c3
Hisha <vndinmchine@gmail.com>
2026-03-09 12:47:54 -0400

    Fixed query

--- bin/Model/PowerPointFiller.php
+++ bin/Model/PowerPointFiller.php
@@ -3,23 +3,24 @@ include_once APP_ROOT . '/bin/Utilities/db.php';
 
 class PowerPointFiller
 {
-    /*
-     * `id` INT NOT NULL AUTO_INCREMENT,
-	 * `program` VARCHAR(50) NOT NULL,
-	 * `title` VARCHAR(50) NOT NULL,
-	 * `pm` VARCHAR(50) NOT NULL,
-	 * `programname` VARCHAR(50) NOT NULL
-     */
-    protected $returnString;
     protected $_returnFields = 'program,title,pm,programname';
     protected $_tableName = 'powerpoint_filler';
     
-    public function getPPFiller(string $selectedProgram){
+    public function getPPFiller(string $selectedProgram)
+    {
         $db = new db();
-        $this->returnString = "SELECT $this->_returnFields FROM $this->_tableName WHERE program = $selectedProgram";
-        $getAll = $db->query($this->returnString)->fetchAll();
+        
+        $sql = "
+            SELECT {$this->_returnFields}
+            FROM {$this->_tableName}
+            WHERE program = ?
+            LIMIT 1
+        ";
+        
+        $result = $db->query($sql, $selectedProgram)->fetchArray();
         $db->close();
-        return $getAll;
+        
+        return $result;
     }
-    
 }
+?>
