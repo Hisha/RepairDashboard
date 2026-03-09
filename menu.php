350544802a8eeb476d257b5cafffbc0e84fb403d
Hisha <vndinmchine@gmail.com>
2026-03-09 10:05:40 -0400

    Menu update

--- menu.php
+++ menu.php
@@ -2,40 +2,7 @@
 ?>
 <table style="background-color: #808080; width: 100%;">
 	<tr>
-		<td width="50%">
-			<table style="width: 100%; border-spacing: 0">
-				<tr>
-					<td align="left"></td>
-					<td align="left"></td>
-				</tr>
-				<tr>
-					<td align="left"></td>
-					<td align="left"></td>
-				</tr>
-			</table>
-		</td>
-		<td width="25%"></td>
-		<td width="25%">
-			<table style="width: 100%; border-spacing: 0">
-				<tr>
-					<td align="right"></td>
-				</tr>
-				<tr>
-					<td align="right"></td>
-				</tr>
-				<tr>
-					<td align="right"></td>
-				</tr>
-			</table>
-		</td>
-	</tr>
-	<tr>
-		<td colspan="3" width="100%">
-			<table style="width: 100%">
-				<tr>
-					<td align="left"><a href="upload_excel.php">Upload Link</a></td>
-				</tr>
-			</table>
-		</td>
+		<a href="index.php">Home</a>
+		<a href="upload_excel.php">Upload Link</a>
 	</tr>
 </table>
