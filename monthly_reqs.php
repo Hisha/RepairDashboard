ba75704c7e775e08557e83e8b9a2ed2f8888ef6c
Hisha <vndinmchine@gmail.com>
2026-03-11 07:21:36 -0400

    Slide to the right

--- monthly_reqs.php
+++ monthly_reqs.php
@@ -185,8 +185,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnGenerateReport']))
             
             $lblReportPeriod = $slide->createRichTextShape()
             ->setHeight(120)
-            ->setWidth(300)
-            ->setOffsetX(500)
+            ->setWidth(350)
+            ->setOffsetX(550)
             ->setOffsetY(140);
             $lblReportPeriod->getActiveParagraph()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
             $lblReportPeriod->createTextRun("Report Period")->getFont()->setName('Calibri')->setBold(true)->setColor(new Color('FF000000'))->setSize(16);
