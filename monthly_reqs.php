6577eb022c02b1a9d3630d6487ff8bebe208bcc0
Hisha <vndinmchine@gmail.com>
2026-03-13 07:56:29 -0400

    Almost there

--- monthly_reqs.php
+++ monthly_reqs.php
@@ -743,7 +743,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnGenerateReport']))
                 550,  // yStart
                 59,   // colWidth
                 20,   // rowHeight
-                180,   // labelWidth
+                190,   // labelWidth
                 $labelAverageColors
                 );
             
