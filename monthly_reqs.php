67a279a70f10ddb739908bfb9e3d60a0c18dc555
Hisha <vndinmchine@gmail.com>
2026-03-13 08:01:08 -0400

    centered?

--- monthly_reqs.php
+++ monthly_reqs.php
@@ -739,7 +739,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnGenerateReport']))
             TableBuilder::renderMonthlyDataTable(
                 $slide3,
                 $tableAverageData,
-                175,  // xStart
+                200,  // xStart
                 560,  // yStart
                 59,   // colWidth
                 20,   // rowHeight
