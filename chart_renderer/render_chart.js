b5bd4c25d6c43c5c1af6bdee3853ead8aa01a296
Hisha <vndinmchine@gmail.com>
2026-03-16 09:14:00 -0400

    Legend fix

--- chart_renderer/render_chart.js
+++ chart_renderer/render_chart.js
@@ -54,35 +54,38 @@ async function main() {
       backgroundColour: 'transparent',
     });
 
-    const configuration = {
-      type: payload.type,
-      data: payload.data,
-      options: {
-        responsive: false,
-        animation: false,
-        plugins: {
-          title: {
-            display: !!payload.title,
-            text: payload.title || '',
-            font: { size: 22 },
-          },
-          legend: {
-            display: payload.legendDisplay !== false,
-            position: payload.legendPosition || 'bottom',
-            labels: {
-              font: { size: 14 }
-            }
-          },
-          tooltip: {
-            enabled: false
-          },
-          ...((payload.options && payload.options.plugins) || {})
-        },
-        scales: payload.scales || {},
-        indexAxis: payload.indexAxis || 'x',
-        ...payload.options
-      }
-    };
+	const payloadOptions = payload.options || {};
+	const payloadPluginOptions = payloadOptions.plugins || {};
+	
+	const configuration = {
+	  type: payload.type,
+	  data: payload.data,
+	  options: {
+	    responsive: false,
+	    animation: false,
+	    ...payloadOptions,
+	    plugins: {
+	      title: {
+	        display: !!payload.title,
+	        text: payload.title || '',
+	        font: { size: 22 },
+	      },
+	      legend: {
+	        display: payload.legendDisplay !== false,
+	        position: payload.legendPosition || 'bottom',
+	        labels: {
+	          font: { size: 14 }
+	        }
+	      },
+	      tooltip: {
+	        enabled: false
+	      },
+	      ...payloadPluginOptions
+	    },
+	    scales: payload.scales || payloadOptions.scales || {},
+	    indexAxis: payload.indexAxis || payloadOptions.indexAxis || 'x'
+	  }
+	};
 
     const image = await chartJSNodeCanvas.renderToBuffer(configuration);
     fs.mkdirSync(path.dirname(output), { recursive: true });
