f77d5855fe5ae660a4b128814d6e79d707466183
Hisha <vndinmchine@gmail.com>
2026-03-16 08:39:53 -0400

    hmmmm

--- monthly_reqs.php
+++ monthly_reqs.php
@@ -1107,7 +1107,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnGenerateReport']))
             $chartAreaX = 70;
             $chartAreaY = 150;
             $chartAreaWidth = 820;
-            $chartAreaHeight = 700;
+            $chartAreaHeight = 750;
             
             $imgInfo = getimagesize($boChartPath);
             
