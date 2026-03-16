bd0750b100f953a54ff142a23ad8b8754c68e682
Hisha <vndinmchine@gmail.com>
2026-03-16 07:05:06 -0400

    Making it pretty

--- menu.php
+++ menu.php
@@ -19,32 +19,37 @@ $lastCMProUpdateFormatted = $lastCMProUpdate
 .menu-bar {
   background-color: #808080;
   padding: 10px;
+  display: flex;
+  align-items: center;
 }
 
-.menu-bar a {
+.menu-links a {
   margin-right: 30px;
   color: black;
   text-decoration: none;
   font-weight: bold;
 }
 
-.menu-status {
-  margin-top: 8px;
-  font-size: 13px;
-  color: black;
+.menu-updates {
+  margin-left: auto;
+  text-align: right;
+  font-size: 12px;
   font-weight: bold;
+  color: black;
 }
 </style>
 
 <div class="menu-bar">
+
+  <div class="menu-links">
     <a href="index.php">Home</a>
     <a href="monthly_reqs.php">Monthly Reqs</a>
     <a href="upload_excel.php">Upload Link</a>
+  </div>
 
-    <div class="menu-status">
-        Last CAVs Update: <?= htmlspecialchars($lastCavsUpdateFormatted) ?>
-    </div>
-    <div class="menu-status">
-        Last CMPro Update: <?= htmlspecialchars($lastCMProUpdateFormatted) ?>
-    </div>
+  <div class="menu-updates">
+    Last CAVs Update: <?= htmlspecialchars($lastCavsUpdate ?? 'N/A') ?><br>
+    Last CMPro Update: <?= htmlspecialchars($lastCmproUpdate ?? 'N/A') ?>
+  </div>
+
 </div>
